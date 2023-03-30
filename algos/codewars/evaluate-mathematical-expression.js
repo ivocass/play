8dbbb0abe977319b50627fe8d9c253c0263c34a4
Ivan Cass <ivoasdf@gmail.com>
2023-03-30 15:18:10 -0300

    updated challenge

--- algos/codewars/evaluate-mathematical-expression.js
+++ algos/codewars/evaluate-mathematical-expression.js
@@ -1,12 +1,5 @@
-/**
- * Improvements:
- * -a better name for 'parenthesesDepth' could be 'nesting'
- * -this returns an array with tokens: expression.match(/([0-9.]+|[-+*()])/g)
- */
-
+// new and improved solution with simplified tokenization (the old one is down below)
 const calc = function (expression) {
-  // match operators and parentheses
-  const regexNaN = /[+\-*/()]/;
   const operations = {
     '+': (a, b) => a + b,
     '-': (a, b) => a - b,
@@ -15,73 +8,37 @@ const calc = function (expression) {
   };
 
   let result;
-  let insideParentheses = false;
-  let parenthesesDepth = 0;
-  let tokens = [];
-  let token = '';
+  let tokens;
 
+  // remove spaces and convert -- to +
   expression = expression.replace(/ /g, '').replace(/--/g, '+');
 
-  // build tokens
-  for (let i = 0; i < expression.length; i++) {
-    const char = expression[i];
+  // tokenize (separate complete numbers from operators and parentheses)
+  tokens = expression.match(/([0-9.]+|[-*+/()])/g);
 
-    if (insideParentheses && char !== '(' && char !== ')') {
-      token += char;
-      continue;
+  // convert expressions inside parentheses into results
+  while (tokens.includes('(')) {
+    // find where parentheses start and (really) end
+    let indexStart = tokens.indexOf('(');
+    let indexEnd = tokens.indexOf(')');
+
+    let indexMid = tokens.indexOf('(', indexStart + 1);
+    while (indexMid > -1 && indexMid < indexEnd) {
+      indexEnd = tokens.indexOf(')', indexEnd + 1);
+      indexMid = tokens.indexOf('(', indexMid + 1);
     }
 
-    if (char === '(') {
-      insideParentheses = true;
+    const parenthesisExpression = tokens.slice(indexStart + 1, indexEnd);
 
-      if (parenthesesDepth > 0) token += '(';
-
-      parenthesesDepth++;
-      continue;
-    }
-
-    if (char === ')') {
-      parenthesesDepth--;
-
-      if (parenthesesDepth === 0) {
-        insideParentheses = false;
-
-        if (regexNaN.test(token)) {
-          token = calc(token);
-        } else {
-          token = Number(token);
-        }
-      } else {
-        token += ')';
-      }
-
-      if (i === expression.length - 1) tokens.push(token);
-
-      continue;
-    }
-
-    // if operand, settle the token as a Number
-    if ('+-/*'.includes(char)) {
-      // handle "*-" and "/-"
-      const prev = expression[i - 1];
-      if (char === '-' && (prev === '*' || prev === '/')) {
-        tokens.push(char);
-        continue;
-      }
-
-      tokens.push(Number(token));
-      token = '';
-
-      tokens.push(char);
-      continue;
-    }
-
-    token += char;
-
-    if (i === expression.length - 1) {
-      tokens.push(Number(token));
-    }
+    tokens = [
+      ...tokens.slice(0, indexStart),
+      calc(parenthesisExpression.join('')),
+      ...tokens.slice(indexEnd + 1),
+    ];
   }
+
+  // handle expressions like "-4" by converting them to "0-4"
+  if (tokens[0] === '-') tokens.unshift(0);
 
   // multiply and divide
   if (tokens.includes('*') || tokens.includes('/')) {
@@ -116,11 +73,14 @@ const calc = function (expression) {
   result = tokens.reduce((acc, token, idx) => {
     if (token !== '+' && token !== '-') return acc;
 
-    return operations[token](acc, tokens[idx + 1]);
+    const right = tokens[idx + 1] === '-' ? 0 : tokens[idx + 1];
+
+    return operations[token](+acc, +right);
   }, tokens[0]);
 
-  return result;
+  return +result;
 };
+
 
 // --------------------------
 function assert(output, expected) {
@@ -128,23 +88,24 @@ function assert(output, expected) {
 }
 
 const tests = [
-  // ['1+1', 2],
-  // ['1 - 1', 0],
-  // ['1* 1', 1],
-  // ['1 /1', 1],
-  // ['-123', -123],
-  // ['123', 123],
-  // ['2 /2+3 * 4.75- -6', 21.25],
-  // ['12* 123', 1476],
-  // ['2 / (2 + 3) * 4.33 - -6', 7.732],
-
-  // ['1+(2*3)/2', 4],
-  // ['2*3+1', 7],
-  // ['2+3*5', 17],
-  // ['2 + (3*5)', 17],
+  ['1+1', 2],
+  ['1 - 1', 0],
+  ['1* 1', 1],
+  ['1 /1', 1],
+  ['-123', -123],
+  ['123', 123],
+  ['2 /2+3 * 4.75- -6', 21.25],
+  ['12* 123', 1476],
+  ['2 / (2 + 3) * 4.33 - -6', 7.732],
+  ['1+(2*3)/2', 4],
+  ['2*3+1', 7],
+  ['2+3*5', 17],
+  ['2 + (3*5)', 17],
   ['(4 + 4 * 3) - 2 / (2 + (3*5)) * 4.33 - -6', 21.49058823529412],
-  // ['8 / (2 + (3*5))', 0.47058823529411764],
-  // ['2*-1', -2],
+  ['8 / (2 + (3*5))', 0.47058823529411764],
+  ['2*-1', -2],
+  ['((80 - (19)))', 61],
+  ['(1 - 2) + -(-(-(-4)))', 3],
 ];
 
 for (const [input, expected] of tests) {
@@ -277,3 +238,126 @@ function calculate(expression) {
   // The result is the only item left on the operand stack
   return operandStack[0];
 }
+
+
+/**
+ * My old solution.
+ * In the new one I simplified the tokenization.
+ */
+const calc = function (expression) {
+  // match operators and parentheses
+  const regexNaN = /[+\-*/()]/;
+  const operations = {
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b,
+    '/': (a, b) => a / b,
+  };
+
+  let result;
+  let insideParentheses = false;
+  let parenthesesDepth = 0;
+  let tokens = [];
+  let token = '';
+
+  expression = expression.replace(/ /g, '').replace(/--/g, '+');
+
+  // build tokens
+  for (let i = 0; i < expression.length; i++) {
+    const char = expression[i];
+
+    if (insideParentheses && char !== '(' && char !== ')') {
+      token += char;
+      continue;
+    }
+
+    if (char === '(') {
+      insideParentheses = true;
+
+      if (parenthesesDepth > 0) token += '(';
+
+      parenthesesDepth++;
+      continue;
+    }
+
+    if (char === ')') {
+      parenthesesDepth--;
+
+      if (parenthesesDepth === 0) {
+        insideParentheses = false;
+
+        if (regexNaN.test(token)) {
+          token = calc(token);
+        } else {
+          token = Number(token);
+        }
+      } else {
+        token += ')';
+      }
+
+      if (i === expression.length - 1) tokens.push(token);
+
+      continue;
+    }
+
+    // if operand, settle the token as a Number
+    if ('+-/*'.includes(char)) {
+      // handle "*-" and "/-"
+      const prev = expression[i - 1];
+      if (char === '-' && (prev === '*' || prev === '/')) {
+        tokens.push(char);
+        continue;
+      }
+
+      tokens.push(Number(token));
+      token = '';
+
+      tokens.push(char);
+      continue;
+    }
+
+    token += char;
+
+    if (i === expression.length - 1) {
+      tokens.push(Number(token));
+    }
+  }
+
+  // multiply and divide
+  if (tokens.includes('*') || tokens.includes('/')) {
+    const tempTokens = [];
+
+    for (let i = 0; i < tokens.length; i++) {
+      const token = tokens[i];
+
+      if (token !== '*' && token !== '/') {
+        tempTokens.push(token);
+        continue;
+      }
+      const operandLeft = tempTokens[tempTokens.length - 1];
+      let operandRight = tokens[i + 1];
+
+      // handle "*-" and "/-"
+      if (operandRight === '-') {
+        operandRight = tokens[i + 2] * -1;
+        i++;
+      }
+
+      const result = operations[token](operandLeft, operandRight);
+
+      tempTokens[tempTokens.length - 1] = result;
+      i++;
+    }
+
+    tokens = tempTokens;
+  }
+
+  // add and subtract
+  result = tokens.reduce((acc, token, idx) => {
+    if (token !== '+' && token !== '-') return acc;
+
+    return operations[token](acc, tokens[idx + 1]);
+  }, tokens[0]);
+
+  return result;
+};
