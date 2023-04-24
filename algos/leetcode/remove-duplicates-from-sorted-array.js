5cee81d356b3f1d18a22565d2828beb7ec24994a
Ivan Cass <ivoasdf@gmail.com>
2023-04-24 16:26:49 -0300

    updated other solution

--- algos/leetcode/remove-duplicates-from-sorted-array.js
+++ algos/leetcode/remove-duplicates-from-sorted-array.js
@@ -55,3 +55,10 @@ assert(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]), null);
 // assert(removeDuplicates([1, 1, 2]), null);
 
 // other solutions -----------------------------------------------------------------------
+var removeDuplicates = function (nums) {
+  let i = 0;
+  for (let j = 1; j < nums.length; j++) {
+    if (nums[j] != nums[i]) nums[++i] = nums[j];
+  }
+  return ++i;
+};
