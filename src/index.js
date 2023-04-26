1665db28486b00037154a870dc4dc02a08566673
Ivan Cass <ivoasdf@gmail.com>
2023-04-25 21:31:22 -0300

    updated assert() to handle arrays

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 // --------------------------
 function assert(output, expected) {
-  console.log('test is', output === expected, output, '/expected:', expected);
+  if (Array.isArray(output) && Array.isArray(expected)) {
+    const isEqualSize = output.length === expected.length;
+    const areValuesEqual = output.every((a, i) => a === expected[i]);
+    console.log(
+      'test is',
+      isEqualSize && areValuesEqual,
+      output,
+      '/expected:',
+      expected
+    );
+  } else {
+    console.log('test is', output === expected, output, '/expected:', expected);
+  }
 }
 
 // assert((), );
