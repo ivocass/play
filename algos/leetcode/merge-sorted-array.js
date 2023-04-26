3e3457683fd69387e2708fe9c4822c63f8b888fe
Ivan Cass <ivoasdf@gmail.com>
2023-04-25 21:50:07 -0300

    added other solution

--- algos/leetcode/merge-sorted-array.js
+++ algos/leetcode/merge-sorted-array.js
@@ -21,10 +21,6 @@ var merge = function (nums1, m, nums2, n) {
       num = temp;
       otherNumIndex = getSwappableIndex(nums1, m, num, startIndex);
 
-      /**
-       * the first index returned in this loop is the pointer we'll use next time
-       * we call getSwappableIndex(), so that we don't start searching from index 0
-       */
       if (iterations === 0) {
         startIndex = otherNumIndex;
       }
@@ -36,10 +32,6 @@ var merge = function (nums1, m, nums2, n) {
       if (otherNumIndex === m) {
         m++;
         break;
-      }
-
-      if (m === nums1.length) {
-        return nums1;
       }
     }
   }
@@ -78,3 +70,40 @@ assert(
 // assert(merge(, , , ), []);
 
 // other solutions -----------------------------------------------------------------------
+
+var merge = function (nums1, m, nums2, n) {
+  // Initialize i and j to store indices of the last element of 1st and 2nd array respectively...
+  let i = m - 1,
+    j = n - 1;
+  // Initialize a variable k to store the last index of the 1st array...
+  let k = m + n - 1;
+  // Create a loop until either of i or j becomes zero...
+  while (i >= 0 && j >= 0) {
+    if (nums1[i] >= nums2[j]) {
+      nums1[k] = nums1[i];
+      i--;
+    } else {
+      nums1[k] = nums2[j];
+      j--;
+    }
+    k--;
+    // Either of i or j is not zero, which means some elements are yet to be merged.
+    // Being already in a sorted manner, append them to the 1st array in the front.
+  }
+  // While i does not become zero...
+  while (i >= 0) nums1[k--] = nums1[i--];
+  // While j does not become zero...
+  while (j >= 0) nums1[k--] = nums2[j--];
+  // Now 1st array has all the elements in the required sorted order...
+  return;
+};
+
+var merge = function (nums1, m, nums2, n) {
+  let idx1 = m - 1;
+  let idx2 = n - 1;
+  let idx3 = m + n - 1;
+
+  while (idx2 >= 0) {
+    nums1[idx3--] = nums1[idx1] > nums2[idx2] ? nums1[idx1--] : nums2[idx2--];
+  }
+};
