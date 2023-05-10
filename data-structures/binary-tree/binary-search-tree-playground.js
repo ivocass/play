9e7efbf3874901daee0da6336369ea641c42ff3d
Ivan Cass <ivoasdf@gmail.com>
2023-05-10 20:43:37 -0300

    updated bst

--- data-structures/binary-tree/binary-search-tree-playground.js
+++ data-structures/binary-tree/binary-search-tree-playground.js
@@ -1,14 +1,18 @@
 import { BinarySearchTree } from './binary-search-tree';
 import { printTree } from './bst-printer';
 
-// create an object for the BinarySearchTree
 var bst = new BinarySearchTree();
 
-// Inserting nodes to the BinarySearchTree
+// bst.fromArray([3, 9, 20, null, null, 15, 7]);
 
-bst.fromArray([3, 9, 20, null, null, 15, 7]);
+// bst.fromArray([5, 4, 7, 2, 11, 14, 6, 18], true);
+// bst.fromArray([1, 2, 2, 3, null, null, 3, 4, null, null, 4]);
 
-console.log('tree', bst.root);
+// console.log('tree', bst.root);
 
-const printed = printTree(bst.root);
-console.log(printed);
+// console.log(printTree(bst.root));
+
+// const res = bst.search(11);
+// console.log('res', res);
+
+// console.log('----------');
--- data-structures/binary-tree/binary-search-tree.js
+++ data-structures/binary-tree/binary-search-tree.js
@@ -1,10 +1,67 @@
 import { TreeNode } from './tree-node';
 
+/**
+ * A BST with iterative and recursive methods for training purposes.
+ */
 export class BinarySearchTree {
   #root = null;
   constructor() {}
 
-  fromArray(array) {
+  /**
+   * @array: example [3, 9, 20, null, null, 15, 7]
+   * @sorts: if true, the tree determines where to place each node. dont use null in this case.
+   * if false, the positions are explicit, and null values are necessary when skipping child nodes.
+   */
+  fromArray(array, sorts = false) {
+    if (sorts) {
+      array.forEach((val) => {
+        this.insert(val);
+      });
+    } else {
+      this.createTree(array);
+    }
+  }
+
+  createTree(array) {
+    return this.createTreeIterative(array);
+  }
+
+  createTreeIterative(array) {
+    this.#root = new TreeNode(array[0]);
+    const q = [this.#root];
+
+    let i = 1;
+    while (q.length > 0 && i < array.length) {
+      const node = q.shift();
+
+      const left = array[i];
+      const right = array[i + 1];
+
+      if (left !== null && left !== undefined) {
+        node.left = new TreeNode(left);
+
+        q.push(node.left);
+      }
+
+      if (right !== null && right !== undefined) {
+        node.right = new TreeNode(right);
+
+        q.push(node.right);
+      }
+
+      i += 2;
+    }
+
+    return this.#root;
+  }
+
+  /**
+   * Not using this one, as it doesn't get along with LeetCode's arrays.
+   * The way it sets the indices doesn't account for the missing children of null nodes,
+   * so for certain cases the indices are out of bounds.
+   * Eg: [1, 2, 2, 3, null, null, 3, 4, null, null, 4] <= the last child node won't be added.
+   */
+  createTreeRecursive(array) {
     function buildTree(index) {
       if (index >= array.length || array[index] === null || array[index] === undefined) {
         return null;
@@ -18,7 +75,7 @@ export class BinarySearchTree {
       return node;
     }
 
-    this.#root = buildTree(0);
+    return buildTree(0);
   }
 
   insert(data) {
@@ -48,7 +105,7 @@ export class BinarySearchTree {
   }
 
   remove(data) {
-    this.#root = this.removeNode(this.#root, data);
+    this.#root = this.#removeNode(this.#root, data);
   }
 
   #removeNode(node, key) {
@@ -100,56 +157,202 @@ export class BinarySearchTree {
     return this.#root;
   }
 
-  // Performs inorder traversal of a tree
-  inorder(node) {
-    if (node !== null) {
-      this.inorder(node.left);
-      console.log(node.data);
-      this.inorder(node.right);
-    }
+  inOrder(prints = true) {
+    return inOrderRecursive(prints);
   }
 
-  // Performs preorder traversal of a tree
-  preorder(node) {
-    if (node !== null) {
-      console.log(node.data);
-      this.preorder(node.left);
-      this.preorder(node.right);
+  inOrderRecursive(prints = true, node = undefined, _output = [], _depth = -1) {
+    _depth++;
+
+    if (node === undefined) {
+      node = this.#root;
     }
+
+    if (node === null) {
+      return _output;
+    }
+
+    this.inOrder(prints, node.left, _output, _depth);
+    _output.push(node.data);
+    this.inOrder(prints, node.right, _output, _depth);
+
+    if (_depth === 0) {
+      return this.#handleOutput(_output, prints);
+    }
+
+    return _output;
   }
 
-  // Performs postorder traversal of a tree
-  // postorder(node) {
-  //   if (node !== null) {
-  //     this.postorder(node.left);
-  //     this.postorder(node.right);
-  //     console.log(node.data);
-  //   }
-  // }
+  inOrderIterative(root) {
+    const res = [];
+    const stack = [];
+    let node = root;
 
-  postorder = (root) => {
-    const nodes = [];
-    if (root) {
-      postorder(root.left);
-      postorder(root.right);
-      nodes.push(root.val);
+    while (node || stack.length) {
+      if (node) {
+        stack.push(node);
+        node = node.left;
+      } else {
+        node = stack.pop();
+        res.push(node.val);
+        node = node.right;
+      }
     }
-    return nodes;
-  };
 
-  search(node, data) {
+    return res;
+  }
+
+  preOrder(prints = true, node = undefined, _output = [], _depth = -1) {
+    _depth++;
+
+    if (node === undefined) {
+      node = this.#root;
+    }
+
+    if (node === null) {
+      return _output;
+    }
+
+    _output.push(node.data);
+    this.preOrder(prints, node.left, _output, _depth);
+    this.preOrder(prints, node.right, _output, _depth);
+
+    if (_depth === 0) {
+      return this.#handleOutput(_output, prints);
+    }
+
+    return _output;
+  }
+
+  postOrder(prints = true, node = undefined, _output = [], _depth = -1) {
+    _depth++;
+
+    if (node === undefined) {
+      node = this.#root;
+    }
+
+    if (node === null) {
+      return _output;
+    }
+
+    this.postOrder(prints, node.left, _output, _depth);
+    this.postOrder(prints, node.right, _output, _depth);
+    _output.push(node.data);
+
+    if (_depth === 0) {
+      return this.#handleOutput(_output, prints);
+    }
+
+    return _output;
+  }
+
+  levelOrder(prints, node) {
+    return levelOrderIterative(prints, node);
+  }
+
+  levelOrderIterative(prints = true, node = undefined) {
+    if (node === undefined) {
+      node = this.#root;
+    }
+
+    if (!node) {
+      return;
+    }
+
+    const output = [];
+    const q = [node];
+
+    while (q.length > 0) {
+      node = q.shift();
+
+      output.push(node.data);
+
+      node.left && q.push(node.left);
+      node.right && q.push(node.right);
+    }
+
+    return this.#handleOutput(output, prints);
+  }
+
+  levelOrderRecursive(prints = true) {
+    const output = [];
+    const height = this.height;
+
+    const process = (node, level) => {
+      if (!node) {
+        return;
+      }
+
+      if (level === 0) {
+        output.push(node.data);
+      } else if (level > 0) {
+        process(node.left, level - 1);
+        process(node.right, level - 1);
+      }
+    };
+
+    for (let i = 0; i < height; i++) {
+      process(this.#root, i);
+    }
+
+    return this.#handleOutput(output, prints);
+  }
+
+  #handleOutput(output, prints) {
+    if (prints) {
+      console.log(output);
+    }
+
+    return output;
+  }
+
+  search(data, node) {
+    if (node === undefined) {
+      node = this.#root;
+    }
+
     if (!node) {
       return null;
     }
 
     if (data < node.data) {
-      return this.search(node.left, data);
+      return this.search(data, node.left);
     }
 
     if (data > node.data) {
-      return this.search(node.right, data);
+      return this.search(data, node.right);
     }
 
     return node;
+  }
+
+  get height() {
+    return this.#height(this.#root);
+  }
+
+  #height(node) {
+    if (!node) {
+      return 0;
+    }
+
+    return 1 + Math.max(this.#height(node.left), this.#height(node.right));
+  }
+
+  isBalanced(node) {
+    if (node === undefined) {
+      node = this.#root;
+    }
+
+    if (!node) {
+      return true;
+    }
+
+    const heightLeft = this.#height(node.left);
+    const heightRight = this.#height(node.right);
+    const heightDiff = Math.abs(heightLeft - heightRight);
+    const isBalancedLeft = this.isBalanced(node.left);
+    const isBalancedRight = this.isBalanced(node.right);
+
+    return heightDiff < 2 && isBalancedLeft && isBalancedRight;
   }
 }
